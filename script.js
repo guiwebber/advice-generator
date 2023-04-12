8b42df14dbb8c2e0b138bfcf7e6343fbc66f2607
Guilherme Webber <guiz1n.webber@gmail.com>
2023-04-11 22:51:10 -0300

    corrigindo js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let url = "https://api.adviceslip.com/advice";
-let frases;
+
 const botao = document.getElementById("slip");
 const texto = document.querySelector(".texto");
 const advice = document.querySelector(".advice");
 let advices = 0;
 
-botao.addEventListener("click", () => {
+
+function chamaFetch() {
   fetch(url).then((response) => {
     response.json().then((data) => {
-      frases = data.slip.advice;
+      let frases = data.slip.advice;
       texto.innerHTML = frases;
       advices = data.slip.id;
       advice.innerHTML = "ADVICE #" + advices;
     });
   });
+}
+
+
+botao.addEventListener("click", () => {
+  chamaFetch();
 });
+chamaFetch();
